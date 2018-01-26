a6d297a56177f67389f99d34062adeb4854b77d6
SUMMIT\cvillanueva <artofartwork@gmail.com>
2018-01-26 15:10:10 -0500

    prueba 2

--- app/src/main/java/com/example/cvillanueva/ejemploretrofit2/MainAdapter.java
+++ app/src/main/java/com/example/cvillanueva/ejemploretrofit2/MainAdapter.java
@@ -14,7 +14,7 @@ import java.util.List;
  * Created by cvillanueva on 26/01/2018.
  */
 
-public class MainAdapter extends RecyclerView.Adapter<MainAdapter.PokemonViewHolder> {
+public class MainAdapter {
 
     private List<Result> list;
     private Context context;
@@ -30,39 +30,14 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.PokemonViewHol
     public void swap(List<Result> newList) {
         list.clear();
         list.addAll(newList);
-        notifyDataSetChanged();
+    //    notifyDataSetChanged();
 
     }
 
-    @Override
-    public PokemonViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return new PokemonViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_pokemon_item, parent, false));
-    }
+   // @Override
+  //  public PokemonViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+     //   return new PokemonViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_pokemon_item, parent, false));
+//    }
 
-    @Override
-    public void onBindViewHolder(PokemonViewHolder holder, final int position) {
-        final Result pokemon = list.(position);
-        holder.tvName.setText(pokemon.getName());
-        holder.tvName.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (view != null) {
-                    view.navigateToDetails(position+1);
-                }
-            }
-        });
-    }
 
-    @Override
-    public int getItemCount() {
-        return list.size();
-    }
-    public static class PokemonViewHolder extends RecyclerView.ViewHolder {
-        TextView tvName;
-
-        public PokemonViewHolder(View itemView) {
-            super(itemView);
-            tvName = (TextView)itemView.findViewById(R.id.tv_name);
-        }
-    }
 }
